53ab5f58ec5a4ac16578b1a76aad38019f8b54cd
yashpatel5566789 <yashpatel5566789@gmail.com>
2019-12-17 18:25:33 +0530

    proxy and server uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://sheltered-scrubland-81633.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
